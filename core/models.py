d965261093f965cd885bd374a337fd024fef0f0a
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-26 20:50:58 -0300

    models criados e adicionados no admin

---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class List(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='usuário')
+    name = models.CharField('nome', max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    List = models.ForeignKey(List, on_delete=models.CASCADE, verbose_name='lista')
+    name = models.CharField('nome', max_length=50)
+    done = models.BooleanField('feito', default=False)
+
+    def __str__(self):
+        return self.name
