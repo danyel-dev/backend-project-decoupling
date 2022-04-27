adf03380f4a689f8974a3fe61b9252df569f57c4
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-26 21:11:05 -0300

    endpoint de listagem das listas criado

---
+++ core/serializers.py
@@ -0,0 +1,8 @@
+from .models import List
+from rest_framework import serializers
+
+
+class ListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = List
+        fields = ['url', 'user', 'name']
