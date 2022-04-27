a87df0827d26bd7a9343826cbcb5116f3f2974a8
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-27 20:30:29 -0300

    alterando o endpoint de listas do usuário para filtrar apenas as listas do usuário que está logado

--- core/views.py
+++ core/views.py
@@ -5,10 +5,12 @@ from .models import List, Item
 
 
 class ListViewSet(viewsets.ModelViewSet):
-    queryset = List.objects.all()
     serializer_class = ListSerializer
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
+
+    def get_queryset(self):
+        return List.objects.filter(user=self.request.user)
 
 
 class ItemViewSet(viewsets.ModelViewSet):
