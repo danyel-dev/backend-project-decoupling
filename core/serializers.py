d5dc0ccf4f36a66192907c0327da9f047acc4842
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-26 21:42:21 -0300

    endpoint de listagem de items criado

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,16 @@
-from .models import List
+from .models import List, Item
 from rest_framework import serializers
 
 
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['List', 'name', 'url', 'done']
+
+
 class ListSerializer(serializers.HyperlinkedModelSerializer):
+    item_set = ItemSerializer(many=True)
+
     class Meta:
         model = List
-        fields = ['url', 'user', 'name']
+        fields = ['name', 'user', 'url', 'item_set']
