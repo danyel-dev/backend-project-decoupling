47b20327c39593dae6e61a7cf8c2b679aa679362
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-27 21:54:43 -0300

    adicionando auth token na url

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
 from core.views import ListViewSet, ItemViewSet
+from rest_framework.authtoken import views
 
 
 router = routers.DefaultRouter()
@@ -13,5 +15,6 @@ router.register(r'item', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token, name='api-tokn-auth'),
     path('admin/', admin.site.urls),
 ]
